3fb33c802be062f71d279a1d2a466283cbcb21f1
Rushui Guan <rushui@outlook.com>
2017-07-26 14:51:31 -0400

    create a code sample on how to use IOC properly with the parser

---
+++ Albatross.Expression.Test/iocSample.cs
@@ -0,0 +1,89 @@
+﻿using Albatross.Expression.Operations;
+using Albatross.Expression.Tokens;
+using NUnit.Framework;
+using SimpleInjector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Albatross.Expression.Test {
+	/// <summary>
+	/// instead of using a default static method to create the parser.  The new Parser concret class
+	/// has a constructor with its operations.  The operations can be preregistered using an IOC container.
+	/// Containers such as SimpleInjector supports Registrations of collections are very handy.
+	/// </summary>
+	[TestFixture]
+	public class iocSample {
+		Container _container;
+
+		[OneTimeSetUp]
+		public void Setup() {
+			_container = new Container();
+			//register the operations collection
+			_container.RegisterCollection<IToken>(
+				new Type[] {
+					typeof(And),
+					typeof(Albatross.Expression.Operations.Array),
+					typeof(Avg),
+					typeof(Coalesce),
+					typeof(CurrentUser),
+					typeof(CurrentMachine),
+					typeof(CurrentApp),
+					typeof(Divide),
+					typeof(Equal),
+					typeof(Format),
+					typeof(GreaterEqual),
+					typeof(GreaterThan),
+					typeof(If),
+					typeof(IsBlank),
+					typeof(LessEqual),
+					typeof(LessThan),
+					typeof(Len),
+					typeof(Max),
+					typeof(Min),
+					typeof(Minus),
+					typeof(Mod),
+					typeof(Month),
+					typeof(MonthName),
+					typeof(Multiply),
+					typeof(Negative),
+					typeof(Not),
+					typeof(NotEqual),
+					typeof(Now),
+					typeof(Or),
+					typeof(PadLeft),
+					typeof(PadRight),
+					typeof(Plus),
+					typeof(Positive),
+					typeof(Power),
+					typeof(ShortMonthName),
+					typeof(Text),
+					typeof(Today),
+					typeof(Year),
+					typeof(Date),
+				}
+			);
+			//register parser and the execution context concrete class
+			_container.Register<IParser, Parser>();
+			_container.Register<IExecutionContext, ExecutionContext>();
+		}
+
+		[Test]
+		public void Test() {
+			IExecutionContext context = _container.GetInstance<IExecutionContext>();
+			object result = context.Eval("1+1", null, typeof(int));
+			Assert.AreEqual(result, 2);
+
+			result = context.Eval("avg(1, 2)", null, typeof(decimal));
+			Assert.AreEqual(result, 1.5);
+		}
+
+
+		[OneTimeTearDown]
+		public void TearDown() {
+			_container.Dispose();
+		}
+	}
+}

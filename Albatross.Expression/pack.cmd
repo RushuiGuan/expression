45bd9c3919f84e30eb7c9c0d3555b2adafa1032c
Rushui Guan <rushui@outlook.com>
2018-01-05 20:24:18 -0500

    convert to the new vs project file format

--- Albatross.Expression/pack.cmd
+++
@@ -1,1 +0,0 @@
-.\.nuget\nuget.exe pack Albatross.Expression.csproj -OutputDirectory c:\Workspace\NuGet\Albatross.Expression -Build -Verbosity detailed -Prop Configuration=Release;OutputPath=bin\Release -Symbols

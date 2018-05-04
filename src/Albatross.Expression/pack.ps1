f97fe0881fbbf7cc6267e7c25e84cb76b5ab6c17
Rushui Guan <rushui@outlook.com>
2018-05-04 17:38:41 -0400

    test commit dev post deployment

--- src/Albatross.Expression/pack.ps1
+++
@@ -1,5 +0,0 @@
-﻿cls
-set-location $PSScriptRoot;
-Set-Alias -Name msbuild -Value "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\bin\msbuild.exe"
-msbuild .\Albatross.Expression.csproj -property:Configuration=Release
-nuget pack .\Albatross.Expression.nuspec -outputDirectory ..\..\pack

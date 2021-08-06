786417c9e30daf060589ebd33ddfc5f7171346f9
MohammedAbdAlrehman <moh.3bd.rhman@gmail.com>
2021-08-06 15:45:37 +0300

    Allow to customize now function to return utc or local date time with hour, minute, second intervals

---
+++ src/Albatross.Expression/Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Albatross.Expression
+{
+    public static class Config
+    {
+        public static class NowFunction
+        {
+            public static DateTimeKind DateTimeKind { get; set; } = DateTimeKind.Local;
+
+            public static int HourInterval { get; set; } = 1;
+
+            public static int MinuteInterval { get; set; } = 1;
+
+            public static int SecoundInterval { get; set; } = 1;
+        }
+    }
+}
--- src/Albatross.Expression/Functions/Date/Now.cs
+++ src/Albatross.Expression/Functions/Date/Now.cs
@@ -1,22 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Albatross.Expression.Tokens;
-using System.Xml;
-using Albatross.Expression.Exceptions;
+﻿using Albatross.Expression.Tokens;
+using System;
 
-namespace Albatross.Expression.Operations {
-	[ParserOperation]
-	public class Now : PrefixOperationToken {
+namespace Albatross.Expression.Operations
+{
+    [ParserOperation]
+    public class Now : PrefixOperationToken
+    {
+        public override string Name { get { return "Now"; } }
+        public override int MinOperandCount { get { return 0; } }
+        public override int MaxOperandCount { get { return 0; } }
+        public override bool Symbolic { get { return false; } }
 
-		public override string Name { get { return "Now"; } }
-		public override int MinOperandCount { get { return 0; } }
-		public override int MaxOperandCount { get { return 0; } }
-		public override bool Symbolic { get { return false; } }
+        public override object EvalValue(Func<string, object> context)
+        {
+            var now = DateTime.Now;
 
-		public override object EvalValue(Func<string, object> context) {
-			return DateTime.Now;
-		}
-	}
+            if (Config.NowFunction.DateTimeKind == DateTimeKind.Utc)
+                now = DateTime.UtcNow;
+
+            var hour = now.Hour;
+            var hourInterval = Config.NowFunction.HourInterval;
+            if (hourInterval <= 0)
+                hour = 0;
+            else
+                hour = (hour / hourInterval) * hourInterval;
+
+            var minute = now.Minute;
+            var minuteInterval = Config.NowFunction.MinuteInterval;
+            if (minuteInterval <= 0)
+                minute = 0;
+            else
+                minute = (minute / minuteInterval) * minuteInterval;
+
+            var second = now.Second;
+            var secondInterval = Config.NowFunction.SecoundInterval;
+            if (secondInterval <= 0)
+                second = 0;
+            else
+                second = (second / secondInterval) * secondInterval;
+
+            return DateTime.SpecifyKind(new DateTime(now.Year, now.Month, now.Day, hour, minute, second), now.Kind);
+        }
+    }
 }

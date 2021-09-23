ae32908d9a2b0bff19db82f2f781328160ece88a
Rushui <rushui@outlook.com>
2021-09-23 16:09:49 -0400

    add random operation

---
+++ src/Albatross.Expression/Operations/Random.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Albatross.Expression.Tokens;
+
+namespace Albatross.Expression.Operations {
+	[ParserOperation]
+	public class Random : PrefixOperationToken {
+
+		public override string Name { get { return "max"; } }
+		public override int MinOperandCount { get { return 2; } }
+		public override int MaxOperandCount { get { return 2; } }
+		public override bool Symbolic { get { return false; } }
+
+
+		public override object EvalValue(Func<string, object> context) {
+			var operands = GetOperands(context);
+			int min = Convert.ToInt32(operands[0]);
+			int max = Convert.ToInt32(operands[1]);
+			return new System.Random().Next(min, max);
+		}
+	}
+}

fea83648ca7a781db28019b7aad72e0b7bbd4b85
Rushui <rguan@welton.com>
2022-04-19 14:36:06 -0400

    add UnixTimestamp2DateTime, which is the reverse of UnixTimestamp

---
+++ src/Albatross.Expression/Operations/UnixTimestamp2DateTime.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using Albatross.Expression.Tokens;
+
+namespace Albatross.Expression.Operations {
+	[ParserOperation]
+	public class UnixTimestamp2DateTime : PrefixOperationToken {
+		public override string Name { get { return "UnixTimestamp2DateTime"; } }
+		public override int MinOperandCount { get { return 1; } }
+		public override int MaxOperandCount { get { return 1; } }
+		public override bool Symbolic { get { return false; } }
+
+		public override object EvalValue(Func<string, object> context) {
+			object value = GetOperands(context).First();
+			long seconds = Convert.ToInt64(value);
+			return DateTimeOffset.FromUnixTimeSeconds(seconds);
+		}
+	}
+}

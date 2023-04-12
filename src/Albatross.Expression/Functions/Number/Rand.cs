ad2d33f7bfb5e9249c789c8ff420886911d2d2a1
MohammedAbdAlrehman <moh.3bd.rhman@gmail.com>
2023-04-12 19:02:05 +0300

    Add rand and randBetween functions

---
+++ src/Albatross.Expression/Functions/Number/Rand.cs
@@ -0,0 +1,35 @@
+﻿using Albatross.Expression.Documentation;
+using Albatross.Expression.Documentation.Attributes;
+using Albatross.Expression.Tokens;
+using System;
+
+namespace Albatross.Expression.Functions.Number
+{
+    [FunctionDoc(Group.Number, "{token}()",
+        @"
+### Generate random value between 0.0 and 1.0
+#### Inputs:
+#### Outputs:
+- Random number between 0.0 and 1.0.
+
+#### References:
+- [{token}](https://help.workiom.com/article/formula#{token})
+        ",
+        @"
+{token}()
+        "
+    )]
+    [ParserOperation]
+    public class Rand : PrefixOperationToken
+    {
+        public override string Name { get { return "Rand"; } }
+        public override int MinOperandCount { get { return 0; } }
+        public override int MaxOperandCount { get { return 0; } }
+        public override bool Symbolic { get { return false; } }
+
+        public override object EvalValue(Func<string, object> context)
+        {
+            return new Random(DateTime.UtcNow.Second).NextDouble();
+        }
+    }
+}
---
+++ src/Albatross.Expression/Functions/Number/RandBetween.cs
@@ -0,0 +1,48 @@
+﻿using Albatross.Expression.Documentation;
+using Albatross.Expression.Documentation.Attributes;
+using Albatross.Expression.Tokens;
+using System;
+using System.Linq;
+
+namespace Albatross.Expression.Functions.Number
+{
+    [FunctionDoc(Group.Number, "{token}(@val1,@val2)",
+        @"
+### Generate random value
+#### Inputs:
+- @val1: Max value, or minimum value of a range (optional)
+- @val2: Maximum value of the range (optional)
+#### Outputs:
+- Non negative random integer.
+
+#### References:
+- [{token}](https://help.workiom.com/article/formula#{token})
+        ",
+        @"
+{token}()
+{token}(1)
+{token}(1,10)
+        "
+    )]
+    [ParserOperation]
+    public class RandBetween : PrefixOperationToken
+    {
+        public override string Name { get { return "RandBetween"; } }
+        public override int MinOperandCount { get { return 0; } }
+        public override int MaxOperandCount { get { return 2; } }
+        public override bool Symbolic { get { return false; } }
+
+        public override object EvalValue(Func<string, object> context)
+        {
+            var rand = new Random(DateTime.UtcNow.Second);
+            var input = Operands.Select(item => (int)Convert.ChangeType(item.EvalValue(context), typeof(int))).ToArray();
+
+            if (input.Length == 1)
+                return (double)rand.Next(input[0]);
+            else if (input.Length == 2)
+                return (double)rand.Next(input[0], input[1]);
+
+            return (double)rand.Next();
+        }
+    }
+}
